22c03e82f85c791a275a2eab7dcb27da1885cc7b
ShiviDev <12shivi.ani@gmail.com>
2023-05-21 22:39:44 +0530

    added

--- test2.js
+++
@@ -1,3 +0,0 @@
-const url = "https://drive.google.com/file/d/1X-oqYaY0cQOkOln10BNgvJBrXixE5t2c/view?usp=share_link";
-const id = url.substring(url.indexOf('/d/') + 3, url.indexOf('/', url.indexOf('/d/') + 3))
-console.log(id);
